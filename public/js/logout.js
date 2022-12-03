8b91bea8d2396150d689caa60172735f9be70caf
Ryan Whitmore <ryan.whit@outlook.com>
2022-12-02 17:31:35 -0800

    Added frontend pages.

---
+++ public/js/logout.js
@@ -0,0 +1,16 @@
+// Content modified from the UCB Gitlab repo:
+// - https://ucb.bootcampcontent.com/UCB-Coding-Bootcamp/UCB-VIRT-FSF-PT-05-2022-U-LOLC/-/tree/main/14-MVC/01-Activities
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert(response.statusText);
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
